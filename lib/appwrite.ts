bc963d9d4c33a94603063135928fdd533f004a14
unknown <elmurodvkh@gmail.com>
2025-01-07 00:01:50 +0500

    setup sdk & auth funtions

---
+++ lib/appwrite.ts
@@ -0,0 +1,96 @@
+import {
+    Account,
+    Avatars,
+    Client,
+    OAuthProvider
+} from "react-native-appwrite"
+import { openAuthSessionAsync } from "expo-web-browser";
+import * as Linking from "expo-linking"
+
+export const config = {
+    platform: "com.restate",
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    galleriesCollectionId:
+        process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    propertiesCollectionId:
+        process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+    bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
+};
+
+export const client = new Client();
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+    try {
+        const redirectUri = Linking.createURL("/");
+
+        const res = await account.createOAuth2Token(
+            OAuthProvider.Google,
+            redirectUri
+        );
+
+        if (!res) throw new Error("Failed to login.");
+
+        const browserResult = await openAuthSessionAsync(
+            res.toString(),
+            redirectUri
+        );
+
+        if (browserResult.type !== "success") throw new Error("Failed to login.")
+
+        const url = new URL(browserResult.url);
+
+        const secret = url.searchParams.get("secret")?.toString();
+        const userId = url.searchParams.get("userId")?.toString();
+
+        if (!secret || !userId) throw new Error("Failed to login.");
+
+        const session = await account.createSession(userId, secret);
+
+        if (!session) throw new Error("Failed to create a session.");
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export async function logout() {
+    try {
+        await account.deleteSession("current");
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export async function getUser() {
+    try {
+        const res = await account.get();
+
+        if (res.$id) {
+            const userAvatar = avatar.getInitials(res.name);
+            return {
+                ...res,
+                avatar: userAvatar.toString()
+            }
+        }
+
+        return null;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
