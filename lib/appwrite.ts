07613ae9be53696f2181265dc003ee2e1aa80895
unknown <elmurodvkh@gmail.com>
2025-01-30 00:39:28 +0500

    details screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -145,3 +145,17 @@ export async function getProperties({ filter, query, limit }: { filter: string; 
         return [];
     }
 }
+
+export async function getPropertyById({ id }: { id: string }) {
+    try {
+        const result = await databases.getDocument(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            id
+        );
+        return result;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
